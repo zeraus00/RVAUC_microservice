ed3c71055c96f472060808815cc6e3493a8e32d2
steins0668 <archelausagaton@gmail.com>
2025-12-02 22:12:00 +0800

    feat(rvauc_ms): add enums and schemas

---
+++ rvauc_ms/enums.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class UniformTypes(Enum):
+    type_a_male = "type_a_male"
---
+++ rvauc_ms/schemas.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from typing import Any, TypedDict
+from pydantic import BaseModel
+
+
+'''
+    Imported schemas from the main server. 
+    Lahat ng nandito nakabase sa schemas doon so ***don't change anything***.
+'''
+
+class ComplianceRecord(TypedDict):
+    '''
+    Schema for the compliance record to be sent to the rvauc-ms server\n
+    Use this ***after*** you confirm the compliance evaluation with yolo.\n
+    Basically ito 'yung laman ng request body for
+    /uniform-compliance/new-record
+    '''
+    studentNumber: str
+    uniformTypeId: int # hardcode to 0 for type A uniform male for now,
+    validFootwear: bool
+    hasId: bool
+    validUpperwear: bool
+    validBottoms: bool
+    termId: int # default to 1 for now
+
+class TokenPayload(BaseModel):
+    '''
+    Pydantic model for schema validation
+    Payload for the access token in the request of the mini web app.
+    '''
+    role: str
+    studentNumber: str
+    department: str
+    yearLevel: str
+    block: str
+
+class ApiResponse(BaseModel):
+    '''
+    The api response type of RvaucMs server
+    '''
+    success: bool
+    message: str
+    result: Any = None
